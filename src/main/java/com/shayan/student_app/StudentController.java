c543902a31e9e5fa6e5f0d1c09422736ca972f49
shayanzarin <shayan.zarin.pars@gmail.com>
2025-11-26 00:15:40 +0100

    Controller for student

---
+++ src/main/java/com/shayan/student_app/StudentController.java
@@ -0,0 +1,29 @@
+package com.shayan.student_app;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    StudentRepo studentRepo;
+
+    @RequestMapping(value = "/students", method = RequestMethod.GET)    
+    public List<Student> getAllStudents() {
+        // return List.of(
+        //     new Student(1, "Alice", 20),
+        //     new Student(2, "Bob", 22),
+        //     new Student(3, "Charlie", 19)
+        // );
+
+        return studentRepo.findAll();
+    }
+    
+}
