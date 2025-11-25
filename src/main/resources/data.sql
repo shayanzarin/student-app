c1dc7a1c3c91782113d4f15655c7fb2f4eed4753
shayanzarin <shayan.zarin.pars@gmail.com>
2025-11-26 00:16:32 +0100

    adding data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO student (name, age) VALUES ('Alice', 22);
+INSERT INTO student (name, age) VALUES ('Bob', 24);
+INSERT INTO student (name, age) VALUES ('Charlie', 23);
+INSERT INTO student (name, age) VALUES ('Diana', 21);
+INSERT INTO student (name, age) VALUES ('Ethan', 25);
+INSERT INTO student (name, age) VALUES ('Fiona', 22);
